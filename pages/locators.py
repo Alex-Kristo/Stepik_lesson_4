36d21a7010b4765ffff72108718fab0400a05f4c
Alexey <50285726+Alex-Kristo@users.noreply.github.com>
2019-07-29 17:04:28 +0300

    Локаторы для продуктовой страницы

--- pages/locators.py
+++ pages/locators.py
@@ -6,5 +6,14 @@ class MainPageLocators(object):
 
 
 class LoginPageLocators(object):
-    LOGIN_FORM = (By.CSS_SELECTOR, '#login_form123')
-    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form123")
+    LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators(object):
+    PRODUCT_LINK = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019'
+    ADD_TO_CARD_BTN = (By.CSS_SELECTOR, "#add_to_basket_form")
+    PRODUCT_NAME = (By.XPATH, '//*[@class="col-sm-6 product_main"]/h1')
+    PRODUCT_PRICE = (By.XPATH, '//*[@class="col-sm-6 product_main"]/p')
+    PRODUCT_NAME_IN_CART = (By.XPATH, '(//*[@class ="alertinner "]/strong)[1]')
+    PRODUCT_PRICE_IN_CART = (By.XPATH, '(//*[@class ="alertinner "]/p/strong)[1]')
