e1218c7258200b05fb43f349d845871acc7275d6
Alexey <50285726+Alex-Kristo@users.noreply.github.com>
2019-07-29 18:04:42 +0300

    Проверка наличия перехода на страницу логина и возможность перехода с продуктовой странице

--- test_product_page.py
+++ test_product_page.py
@@ -24,3 +24,24 @@ def test_test_guest_can_add_product_to_cart(driver, link):
         page.find_product_name(*ProductPageLocators.PRODUCT_NAME), "Product name does not match"
     assert page.find_product_price(*ProductPageLocators.PRODUCT_PRICE_IN_CART) == \
         page.find_product_price(*ProductPageLocators.PRODUCT_PRICE), "Product price does not match"
+
+
+def test_guest_cant_see_success_message(driver):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+    page = ProductPage(driver, link)
+    page.open()
+    page.is_not_element_present(*ProductPageLocators.ADD_TO_CART_ACCESS)
+
+
+def test_guest_should_see_login_link_on_product_page(driver):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(driver, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(driver):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(driver, link)
+    page.open()
+    page.should_be_login_link()
