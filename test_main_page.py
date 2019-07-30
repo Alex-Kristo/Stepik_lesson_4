de6b45d564f9c1fcb161c4fd6b7e22e840166f85
Alexey <50285726+Alex-Kristo@users.noreply.github.com>
2019-07-30 15:29:41 +0300

    Проверит отстутствие  товаров в корзине

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,11 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+from .pages.locators import MainPageLocators
+from .pages.cart_page import CartPage
 
 
 def test_guest_can_go_to_login_page(driver):
-    link = "http://selenium1py.pythonanywhere.com"
-    page = MainPage(driver, link)
+    page = MainPage(driver, MainPageLocators.MAIN_PAGE_LINK)
     page.open()
     page.go_to_login_page()
     login_page = LoginPage(driver, driver.current_url)
@@ -12,8 +13,13 @@ def test_guest_can_go_to_login_page(driver):
 
 
 def test_guest_should_see_login_link(driver):
-    link = 'http://selenium1py.pythonanywhere.com'
-    page = MainPage(driver, link)
+    page = MainPage(driver, MainPageLocators.MAIN_PAGE_LINK)
     page.open()
     page.should_be_login_link()
 
+
+def test_guest_cant_see_product_in_cart_opened_from_main_page(driver):
+    page = MainPage(driver, MainPageLocators.MAIN_PAGE_LINK)
+    base_page_cart = page.go_to_cart()
+    page_cart = CartPage(base_page_cart.driver, base_page_cart.url)
+    page_cart.should_be_empty_cart()
