0b8e8ca74f5f2e166269e6a71242184770ffd2cd
Alexey <50285726+Alex-Kristo@users.noreply.github.com>
2019-07-30 15:34:28 +0300

    Добавит локаторы страницы корзины

--- pages/locators.py
+++ pages/locators.py
@@ -4,10 +4,12 @@ from selenium.webdriver.common.by import By
 class BasePageLocators(object):
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    CART_BTN = (By.XPATH, "//span/a")
 
 
 class MainPageLocators(object):
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    MAIN_PAGE_LINK = "http://selenium1py.pythonanywhere.com"
 
 
 class LoginPageLocators(object):
@@ -17,9 +19,15 @@ class LoginPageLocators(object):
 
 class ProductPageLocators(object):
     PRODUCT_LINK = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019'
-    ADD_TO_CARD_BTN = (By.CSS_SELECTOR, "#add_to_basket_form")
+    ADD_TO_CART_BTN = (By.CSS_SELECTOR, "#add_to_basket_form")
     PRODUCT_NAME = (By.XPATH, '//*[@class="col-sm-6 product_main"]/h1')
     PRODUCT_PRICE = (By.XPATH, '//*[@class="col-sm-6 product_main"]/p')
     PRODUCT_NAME_IN_CART = (By.XPATH, '(//*[@class ="alertinner "]/strong)[1]')
     PRODUCT_PRICE_IN_CART = (By.XPATH, '(//*[@class ="alertinner "]/p/strong)[1]')
     ADD_TO_CART_ACCESS = (By.XPATH, '(//*[@class ="alertinner "])[1]')
+
+
+class CartPageLocators(object):
+    PRODUCTS_IN_CART = (By.CSS_SELECTOR, '.col-sm-6.h3')
+    EMPTY_CART_MESSAGE = (By.CSS_SELECTOR, '#content_inner > p')
+    CART_LINK = "http://selenium1py.pythonanywhere.com/en-gb/basket/"
