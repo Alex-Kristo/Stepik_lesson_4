bad3bca5d282fd1e0db982e1a195ac64410b8446
Alexey <50285726+Alex-Kristo@users.noreply.github.com>
2019-07-30 19:28:50 +0300

    Объеденит тесты в группу

--- test_main_page.py
+++ test_main_page.py
@@ -1,21 +1,23 @@
+import pytest
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 from .pages.locators import MainPageLocators
 from .pages.cart_page import CartPage
 
 
-def test_guest_can_go_to_login_page(driver):
-    page = MainPage(driver, MainPageLocators.MAIN_PAGE_LINK)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(driver, driver.current_url)
-    login_page.should_be_login_page()
+@pytest.mark.login_guest
+class TestLoginFromMainPage(object):
+    def test_guest_can_go_to_login_page(self, driver):
+        page = MainPage(driver, MainPageLocators.MAIN_PAGE_LINK)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(driver, driver.current_url)
+        login_page.should_be_login_page()
 
-
-def test_guest_should_see_login_link(driver):
-    page = MainPage(driver, MainPageLocators.MAIN_PAGE_LINK)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_should_see_login_link(self, driver):
+        page = MainPage(driver, MainPageLocators.MAIN_PAGE_LINK)
+        page.open()
+        page.should_be_login_link()
 
 
 def test_guest_cant_see_product_in_cart_opened_from_main_page(driver):
