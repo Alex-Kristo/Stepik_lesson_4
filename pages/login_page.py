ac9e9def5c8c3bfcdb6762bf431f1c6db88ddf44
Alexey <50285726+Alex-Kristo@users.noreply.github.com>
2019-07-30 15:33:37 +0300

    Исправит проверку страницы логина

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,10 +1,5 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
-from .locators import MainPageLocators
-
-
-def test_substring(full_string, substring):
-    assert substring in full_string, "expected '{}' to be substring of '{}'".format(substring,full_string)
 
 
 class LoginPage(BasePage):
@@ -15,14 +10,11 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         current_url = self.driver.current_url
-        print(current_url)
-        test_substring(current_url, 'login')
-        assert True
+        self.substring_in(current_url, 'login')
+        assert True, "Login url is not presented"
 
     def should_be_login_form(self):
-        self.is_element_present(*LoginPageLocators.LOGIN_FORM)
-        assert True
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
     def should_be_register_form(self):
-        self.is_element_present(*LoginPageLocators.REGISTER_FORM)
-        assert True
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
