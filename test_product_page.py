808a2ca06bf8538fb7d89deef890295f31e042db
Alexey <50285726+Alex-Kristo@users.noreply.github.com>
2019-07-30 19:52:24 +0300

    Отмаркерует тесты. Уберет assert

--- test_product_page.py
+++ test_product_page.py
@@ -17,6 +17,7 @@ class TestUserAddToCartFromProductPage(object):
         page.register_new_user(email, password)
         page.should_be_authorized_user()
 
+    @pytest.mark.need_review
     @pytest.mark.parametrize('link',
                              ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"])
     def test_user_can_add_product_to_cart(self, driver, link):
@@ -38,6 +39,7 @@ class TestUserAddToCartFromProductPage(object):
         page.is_not_element_present(*ProductPageLocators.ADD_TO_CART_ACCESS)
 
 
+@pytest.mark.need_review
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"])
 def test_guest_can_add_product_to_cart(driver, link):
     page = ProductPage(driver, link)
@@ -46,10 +48,8 @@ def test_guest_can_add_product_to_cart(driver, link):
     add_to_cart_btn = page.driver.find_element(*ProductPageLocators.ADD_TO_CART_BTN)
     add_to_cart_btn.click()
     page.solve_quiz_and_get_code()
-    assert page.find_product_name(*ProductPageLocators.PRODUCT_NAME_IN_CART) == \
-        page.find_product_name(*ProductPageLocators.PRODUCT_NAME), "Product name does not match"
-    assert page.find_product_price(*ProductPageLocators.PRODUCT_PRICE_IN_CART) == \
-        page.find_product_price(*ProductPageLocators.PRODUCT_PRICE), "Product price does not match"
+    page.product_name_in_cart()
+    page.product_price_in_cart()
 
 
 def test_guest_cant_see_success_message(driver):
@@ -66,6 +66,7 @@ def test_guest_should_see_login_link_on_product_page(driver):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(driver):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(driver, link)
@@ -73,6 +74,7 @@ def test_guest_can_go_to_login_page_from_product_page(driver):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_cart_opened_from_product_page(driver):
     page = ProductPage(driver, ProductPageLocators.PRODUCT_LINK)
     base_page_cart = page.go_to_cart()
