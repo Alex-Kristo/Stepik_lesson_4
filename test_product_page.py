404c71ec6d0a638ce1466f89a1a49ffab6cee640
Alexey <50285726+Alex-Kristo@users.noreply.github.com>
2019-07-30 20:24:34 +0300

    Убирет линки в локаторы

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,7 @@
 import pytest
 from .pages.product_page import ProductPage
 from .pages.locators import ProductPageLocators
+from .pages.locators import LoginPageLocators
 from .pages.cart_page import CartPage
 from .pages.login_page import LoginPage
 import time
@@ -9,8 +10,7 @@ import time
 class TestUserAddToCartFromProductPage(object):
     @pytest.fixture(scope="function", autouse=True)
     def setup(self, driver):
-        link = "http://selenium1py.pythonanywhere.com/accounts/login/"
-        page = LoginPage(driver, link)
+        page = LoginPage(driver, LoginPageLocators.LOGIN_LINK)
         page.open()
         email = str(time.time()) + "@fakemail.org"
         password = str(time.time()) + "@fakemail.org"
@@ -33,8 +33,7 @@ class TestUserAddToCartFromProductPage(object):
             page.find_product_price(*ProductPageLocators.PRODUCT_PRICE), "Product price does not match"
 
     def test_user_cant_see_success_message(self, driver):
-        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
-        page = ProductPage(driver, link)
+        page = ProductPage(driver, ProductPageLocators.PRODUCT_LINK)
         page.open()
         page.is_not_element_present(*ProductPageLocators.ADD_TO_CART_ACCESS)
 
@@ -53,23 +52,20 @@ def test_guest_can_add_product_to_cart(driver, link):
 
 
 def test_guest_cant_see_success_message(driver):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
-    page = ProductPage(driver, link)
+    page = ProductPage(driver, ProductPageLocators.PRODUCT_LINK)
     page.open()
     page.is_not_element_present(*ProductPageLocators.ADD_TO_CART_ACCESS)
 
 
 def test_guest_should_see_login_link_on_product_page(driver):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(driver, link)
+    page = ProductPage(driver, ProductPageLocators.PRODUCT_LINK)
     page.open()
     page.should_be_login_link()
 
 
 @pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(driver):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(driver, link)
+    page = ProductPage(driver, ProductPageLocators.PRODUCT_LINK)
     page.open()
     page.should_be_login_link()
 
